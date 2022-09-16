3a51e76ed6b82fa8a4696e0bab267efa27a5b1f1
Alexandros Fotiadis <fotiadis@alexandros.blue>
2022-09-16 15:15:18 +0300

    fix: Upload placeholder does not get removed after finish upload #2849

--- packages/drag-n-drop-upload/src/components/insertPlaceholder.ts
+++ packages/drag-n-drop-upload/src/components/insertPlaceholder.ts
@@ -1,13 +1,16 @@
 import { EditorState, AtomicBlockUtils } from 'draft-js';
 
 type InsertPlaceholderReturn = {
-  state: EditorState,
-  blockKey: string,
-  key: string,
-  text: string
+  state: EditorState;
+  blockKey: string;
+  key: string;
+  text: string;
 };
 
-export const insertPlaceholder = (editorState:EditorState, text:string): InsertPlaceholderReturn => {
+export const insertPlaceholder = (
+  editorState: EditorState,
+  text: string
+): InsertPlaceholderReturn => {
   const contentState = editorState.getCurrentContent();
   const contentStateWithEntity = contentState.createEntity(
     'UploadPlaceholder',
@@ -21,18 +24,23 @@ export const insertPlaceholder = (editorState:EditorState, text:string): InsertP
     ' '
   );
 
+  const insertedAtomicBlock = newEditorState
+    .getCurrentContent()
+    .getBlockBefore(newEditorState.getSelection().getAnchorKey());
+
+  if (insertedAtomicBlock === undefined) {
+    throw new Error('Unable to locate the block-key of inserted-atomic-block.');
+  }
+
   const stateSelected = EditorState.forceSelection(
     newEditorState,
     newEditorState.getCurrentContent().getSelectionAfter()
   );
 
-  const blockArrayMap = newEditorState.getCurrentContent().getBlocksAsArray().map(b => b.getKey());
-  const nextToLastBlock = newEditorState.getCurrentContent().getBlockForKey(blockArrayMap[blockArrayMap.length-2]);
-
   return {
     state: stateSelected,
-    blockKey: nextToLastBlock.getKey(),
+    blockKey: insertedAtomicBlock.getKey(),
     key: entityKey,
-    text
-  }
+    text,
+  };
 };
